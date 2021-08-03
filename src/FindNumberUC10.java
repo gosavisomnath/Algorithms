fb2af56e7a3231bafea04f1b89baad7048236786
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-08-03 14:03:13 +0530

    Code Added

---
+++ src/FindNumberUC10.java
@@ -0,0 +1,49 @@
+//Algorithm:
+/*
+suppose number = 2;
+n =2^n = 4;
+higher 4 so print num is bet 0 to 4
+mid form = 0 + 4/2 = 2;
+high - low == 1 ; 4-0 = 4; 4 == 1;
+condition false
+if boolean condition
+asked low or mid now low 1 nd high 4
+4 > 1 correct print mid 2 this is our number
+if this condition not true it is going to else part
+// enter input 2
+think number 0 to 4
+number less than 2
+false
+number is less than 3
+true
+number get 2
+*/
+import java.util.Scanner;
+
+public class FindNumberUC10 {
+    // create a constructor
+    public static int guessing(int low, int high) {
+        Scanner sc = new Scanner(System.in);
+        int mid = (low + high) / 2;
+        if (high - low == 1)
+            return low;
+        System.out.println("this number is less than from this number  " + mid);
+        if (sc.nextBoolean()) {
+            return guessing(low, mid);
+        } else {
+            return guessing(mid, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the input ");
+        int n = sc.nextInt();
+        int high = (int) Math.pow(2, n);
+        int low = 0;
+        System.out.println("think the number in between " + low + "to" + high);
+        System.out.println("number is " + guessing(low, high));
+        sc.close();
+    }
+
+}
---
+++ src/PrimeNumberUC7UC8.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class PrimeNumberUC7UC8
+{
+    public static void main(String[] args)
+    {
+        //scanner class object creation
+        Scanner sc=new Scanner(System.in);
+        //input from user
+        System.out.print("Enter Starting Number : ");
+        int start = 0;
+        System.out.print("Enter Ending Number : ");
+        int end = 1000;
+        System.out.println("Prime numbers between "+start+" and "+end+" are : ");
+        int count = 0;
+        //loop for finding and printing all prime numbers between given range
+        for(int i = start ; i <= end ; i++) {
+            //logic for checking number is prime or not
+            count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0)
+                    count = count + 1;
+            }
+            if (count == 2)
+                System.out.println(i);
+
+        }
+        // check palindromes
+
+        //declare and initialize variables
+        int  temp,i;
+        int sum = 0;
+
+        System.out.println("Enter number to check for palindrome");
+        //get input from user
+        i = sc.nextInt();
+
+        //store number in a temporary variable temp
+        temp = i;
+
+        //use for loop check whether number is prime or not
+        for(i = 1; i <= temp; i++)
+        {
+            if(temp%i == 0)
+            {
+                count++;    //increment counter when the reminder is 0
+            }
+        }
+
+        //use while loop to check whether the number is palindrome or not
+        while(i > 0)
+        {
+            // get last digit of the number
+            int rem = i % 10;
+
+            // store the digit last digit
+            sum = sum*10+rem;
+
+            // extract all digit except the last
+            i = i/10;
+        }
+
+        //check whether the number is palindrome and Prime or not
+        if(temp == sum && count == 2)
+        {
+            System.out.println(temp +" is a PalPrime number");
+        }
+        else
+        {
+            System.out.println(temp +" is not a PalPrime number");
+        }
+
+    }
+}
